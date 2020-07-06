c60ab1bdcd2ea0920f7b0f8dff5748124be21957
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-06 16:50:31 +0300

    waypoint system added, splash screen added

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene", 3f);
+    }
+    
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
