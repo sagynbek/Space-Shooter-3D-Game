0e1770f23fffb800379f4f84faf9df28dcc87b44
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-07 09:53:37 +0300

    Player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 15f;
+    [Tooltip("In m/s")] [SerializeField] float xClamp = 5f;
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 5f;
+    [Tooltip("In m/s")] [SerializeField] float yClamp = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = horizontalThrow * Time.deltaTime * xSpeed;
+        float yOffset = verticalThrow * Time.deltaTime * ySpeed;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xClamp, xClamp);
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yClamp, yClamp);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
