e27a0e290d905f618e1c844ac8b973d41f908cb4
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-07-07 11:20:44 +0300

    Rotate player wıth movemnt

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -7,8 +8,22 @@ public class Player : MonoBehaviour
 {
     [Tooltip("In m/s")][SerializeField] float xSpeed = 15f;
     [Tooltip("In m/s")] [SerializeField] float xClamp = 5f;
+
     [Tooltip("In m/s")] [SerializeField] float ySpeed = 5f;
     [Tooltip("In m/s")] [SerializeField] float yClamp = 3f;
+
+    [Tooltip("In m/s")] [SerializeField] float zSpeed = 60f;
+    [Tooltip("In m/s")] [SerializeField] float zClamp = 60f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+
+    [SerializeField] float positionYawFactor = 5f;
+
+    [SerializeField] float controlRollFactor = -20f;
+    
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +34,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        float xOffset = horizontalThrow * Time.deltaTime * xSpeed;
-        float yOffset = verticalThrow * Time.deltaTime * ySpeed;
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y*positionPitchFactor + yThrow*controlPitchFactor;
+
+        float yaw = transform.localPosition.x*positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+
+        //float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        //float rawNewZRot = horizontalThrow * zSpeed* Time.deltaTime + transform.localRotation.z;
+        //Debug.Log(rawNewZRot + " " +transform.localRotation.z);
+
+        //float clampedZRot = Mathf.Clamp(rawNewZRot, -zClamp, zClamp);
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * xSpeed;
+        float yOffset = yThrow * Time.deltaTime * ySpeed;
 
         float rawNewXPos = transform.localPosition.x + xOffset;
         float rawNewYPos = transform.localPosition.y + yOffset;
@@ -33,4 +73,5 @@ public class Player : MonoBehaviour
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
+
 }
